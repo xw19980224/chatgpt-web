ff4ee11bafe30fadfa0d9a25cb854d848416bcc3
maxwell <1035071992@qq.com>
2023-04-08 12:01:40 +0800

    perf: 调整helper文件

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -16,8 +16,8 @@ export function defaultSetting(): UserState {
   return {
     userInfo: {
       avatar: 'https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg',
-      name: 'ChenZhaoYu',
-      description: 'Star on <a href="https://github.com/Chanzhaoyu/chatgpt-bot" class="text-blue-500" target="_blank" >Github</a>',
+      name: 'maxwell',
+      description: 'Star on <a href="https://github.com/xw19980224/chatgpt-web" class="text-blue-500" target="_blank" >Github</a>',
     },
   }
 }
